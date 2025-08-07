2c75ef3d8ef543345a63e885d2ff9a50293bf252
alsueliyevas <alsualiyeva2007@gmail.com>
2025-08-07 16:26:23 -0700

    added project

---
+++ production.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS cheese_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Domain TEXT,
+    Value INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS honey_production (
+    Year INTEGER,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Value INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS milk_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Domain TEXT,
+    Value INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS coffee_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,
+    Commodity_ID INTEGER,
+    Value INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS egg_production (
+    Year INTEGER,
+    Period TEXT,
+    Geo_Level TEXT,
+    State_ANSI INTEGER,_
+);
+
